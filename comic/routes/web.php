283ef3d24d07c934e61786f4fc0f28fafe4d0f2d
LuongoAndrea <andrea.luongo2002@gmail.com>
2022-12-18 23:43:01 +0100

    foreach

--- comic/routes/web.php
+++ comic/routes/web.php
@@ -14,5 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $comics = config('dbcomic.comic');
+    // dd(compact('comics'));
+    return view('home', compact('comics'));
 });
